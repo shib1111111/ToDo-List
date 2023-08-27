63caec710c5aa1e8ad44cedf28c966fa73a01726
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-08-27 13:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ login_manager = LoginManager(app)
 # Database Models
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(200), nullable=False)
 
